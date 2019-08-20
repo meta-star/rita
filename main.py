0d229747046ff044a669e5d65c9870bbb35903ea
SuperSonic <supersonic@livemail.tw>
2019-08-20 13:15:17 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+
+import sys
+import numpy
+
+def main(msgs):
+    result = readCommandLine(msgs)
+    print(result)
+
+def readCommandLine(msgs):
+    replymsg = ""
+    for msg in msgs:
+        replymsg = replymsg + " " + msg
+    return replymsg
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
